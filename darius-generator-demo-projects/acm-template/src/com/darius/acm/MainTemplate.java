052e13dd9814143d612ef7477d1528ce18c4307d
heyufan1 <3045880086@qq.com>
2026-04-15 14:38:51 +0800

    test

---
+++ darius-generator-demo-projects/acm-template/src/com/darius/acm/MainTemplate.java
@@ -0,0 +1,25 @@
+package com.darius.acm
+
+public class MainTemplate{
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()) {
+            int a = sc.nextInt();
+
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            int sum = 0;
+            for(int num : arr) {
+                sum += num;
+            }
+
+            System.out.println(sum);
+        }
+        sc.close();
+    }
+}
